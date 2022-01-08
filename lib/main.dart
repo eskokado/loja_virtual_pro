7d1db9cafdac3fe89e051f6a89758af07db72010
eskokado <eskokado@gmail.com>
2022-01-08 15:38:11 -0300

    feat: config firebase android

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
   runApp(const MyApp());
+  await Firebase.initializeApp();
+  FirebaseFirestore.instance.collection('teste').add({'teste': 'teste'});
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-
   @override
   Widget build(BuildContext context) {
-    if (true) 
-      debugPrint('teste');
-    
+    if (true) debugPrint('teste');
 
     return MaterialApp(
       title: 'Flutter Demo',
