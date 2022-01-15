a464ae9eb470abe93905b1ff26cb8578ea384a96
eskokado <eskokado@gmail.com>
2022-01-15 19:19:09 -0300

    feat: carregar o usuario ao abrir o app

--- lib/models/user_manager.dart
+++ lib/models/user_manager.dart
@@ -5,16 +5,23 @@ import '../helpers/firebase_errors.dart';
 import 'user.dart';
 
 class UserManager extends ChangeNotifier {
+  UserManager() {
+    _loadCurrentUser();
+  }
+
   final Fire.FirebaseAuth auth = Fire.FirebaseAuth.instance;
 
-  bool loading = false;
+  bool _loading = false;
+  bool get loading => _loading;
+
+  Fire.User? userFire;
 
   Future<void> signIn({
     required User user,
     required Function onFail,
     required Function onSuccess,
   }) async {
-    setLoading(true);
+    loading = true;
     try {
       final Fire.UserCredential result = await auth.signInWithEmailAndPassword(
         email: user.email,
@@ -23,16 +30,27 @@ class UserManager extends ChangeNotifier {
 
       await Future.delayed(Duration(seconds: 5));
 
+      userFire = result.user;
+
       onSuccess(result.user!.uid);
     } on Fire.FirebaseAuthException catch (e) {
       //print(e.code);
       onFail(getErrorString(e.code));
     }
-    setLoading(false);
+    loading = false;
   }
 
-  void setLoading(bool value) {
-    loading = value;
+  set loading(bool value){
+    _loading = value;
+    notifyListeners();
+  }
+
+  Future<void> _loadCurrentUser() async {
+    final Fire.User? currentUser = auth.currentUser;
+    if (currentUser != null) {
+      userFire = currentUser;
+      print(userFire!.uid);
+    }
     notifyListeners();
   }
 }
