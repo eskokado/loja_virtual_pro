9c9824c5a3301d4bfbd10eed7c2a014f3b4412a6
eskokado <eskokado@gmail.com>
2022-02-15 06:31:37 -0300

    feat: display the image in form

--- lib/screens/edit_product/components/images_form.dart
+++ lib/screens/edit_product/components/images_form.dart
@@ -1,17 +1,73 @@
-import 'package:flutter/material.dart';
-import 'package:loja_virtual_pro/models/product.dart';
+// ignore_for_file: unnecessary_statements
 
-class ImagesForm extends StatelessWidget {
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:infinite_carousel/infinite_carousel.dart';
+
+import '../../../models/product.dart';
+
+class ImagesForm extends StatefulWidget {
   const ImagesForm({Key? key, required this.product}) : super(key: key);
 
   final Product product;
 
   @override
+  State<ImagesForm> createState() => _ImagesFormState();
+}
+
+class _ImagesFormState extends State<ImagesForm> {
+  int _selectedIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
     return FormField<List<dynamic>>(
-      initialValue: product.images,
+      initialValue: widget.product.images,
       builder: (state) {
-        return Container();
+        return AspectRatio(
+          aspectRatio: 1,
+          child: InfiniteCarousel.builder(
+            itemCount: state.value == null ? 0 : state.value!.length,
+            itemExtent: MediaQuery.of(context).size.width,
+            onIndexChanged: (index) {
+              if (_selectedIndex != index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              }
+            },
+            itemBuilder: (context, itemIndex, realIndex) {
+              return Stack(
+                fit: StackFit.expand,
+                children: [
+                  if (state.value![itemIndex] is String)
+                    Image.network(
+                      state.value![itemIndex] as String,
+                      fit: BoxFit.cover,
+                    )
+                  else
+                    Image.file(
+                      state.value![itemIndex] as File,
+                      fit: BoxFit.cover,
+                    ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      icon: const Icon(Icons.remove),
+                      color: Colors.red,
+                      onPressed: () {
+                        if (state.value![itemIndex] != null) {
+                          state.value!.remove(state.value![itemIndex]);
+                          state.didChange(state.value);
+                        }
+                      },
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
+        );
       },
     );
   }
