d7a814ea3c7e5ecc8919203b721bbba9b9472431
eskokado <eskokado@gmail.com>
2022-02-15 05:40:53 -0300

    fix: ajust alphabetics users

--- lib/screens/admin_users/admin_users_screen.dart
+++ lib/screens/admin_users/admin_users_screen.dart
@@ -1,4 +1,4 @@
-import 'package:alphabet_list_scroll_view/alphabet_list_scroll_view.dart';
+import 'package:alphabet_scroll_view/alphabet_scroll_view.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -18,13 +18,44 @@ class AdminUsersScreen extends StatelessWidget {
       ),
       body: Consumer<AdminUsersManager>(
         builder: (_, adminUsersManager, __) {
-          return AlphabetListScrollView(
-            itemBuilder: (_, index) {
+          return AlphabetScrollView(
+            list: adminUsersManager.names.map((e) => AlphaModel(e)).toList(),
+            alignment: LetterAlignment.right,
+            unselectedTextStyle:
+                const TextStyle(color: Colors.white, fontSize: 20),
+            selectedTextStyle:
+                const TextStyle(color: Colors.white, fontSize: 24),
+            overlayWidget: (value) => Stack(
+              alignment: Alignment.center,
+              children: [
+                const Icon(
+                  Icons.star,
+                  size: 50,
+                  color: Colors.red,
+                ),
+                Container(
+                  height: 50,
+                  width: 50,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    // color: Theme.of(context).primaryColor,
+                  ),
+                  alignment: Alignment.center,
+                  child: Text(
+                    value.toUpperCase(),
+                    style: const TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+            itemBuilder: (_, index, id) {
               return ListTile(
                 title: Text(
                   adminUsersManager.users[index].name ?? '',
                   style: const TextStyle(
-                      fontWeight: FontWeight.w800, color: Colors.white),
+                    fontWeight: FontWeight.w800,
+                    color: Colors.white,
+                  ),
                 ),
                 subtitle: Text(
                   adminUsersManager.users[index].email ?? '',
@@ -34,11 +65,6 @@ class AdminUsersScreen extends StatelessWidget {
                 ),
               );
             },
-            highlightTextStyle:
-                const TextStyle(color: Colors.white, fontSize: 20),
-            indexedHeight: (index) => 80,
-            strList: adminUsersManager.names,
-            showPreview: true,
           );
         },
       ),
